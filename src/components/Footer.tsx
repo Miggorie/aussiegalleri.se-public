0ac5eb77bd16978c8fe714516897a34261c5c7cc
Miggorie <info@elinselin.com>
2023-01-05 00:55:29 +0100

    Creating a footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterSocialContainer,
+  FooterSocialLink,
+  FooterText,
+} from "./FooterStyles";
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSocialContainer>
+        <FooterSocialLink href="#">
+          <FaFacebookSquare size={24} />
+        </FooterSocialLink>
+        <FooterSocialLink href="#">
+          <FaInstagram size={24} />
+        </FooterSocialLink>
+      </FooterSocialContainer>
+      <FooterText>Copyright 2021</FooterText>
+    </FooterContainer>
+  );
+};
 
 export default Footer;
---
+++ src/components/FooterStyles.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+`;
+
+export const FooterSocialContainer = styled.div`
+  display: flex;
+`;
+
+export const FooterSocialLink = styled.a`
+  display: block;
+  padding: 0.5rem;
+  color: #fff;
+`;
+
+export const FooterText = styled.div`
+  font-size: 0.875rem;
+`;
